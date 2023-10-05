869cbd16bca10295e1d99a81c7c71b3f1a7fb6e0
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-10-05 14:11:11 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NavBar } from "./NavBar.tsx";
+import { Product } from "./Product.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NavBar />
+    <Product />
   </React.StrictMode>
 );
