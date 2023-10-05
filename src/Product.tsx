e096acfa1966846af9fe7b7af9e34764e16d650e
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-10-05 16:31:48 -0700

    Add working count input

--- src/Product.tsx
+++ src/Product.tsx
@@ -33,42 +33,49 @@ export function Product() {
             });
     }, []); // Empty dependency array ensures this effect runs once when the component mounts
 
+    const [quantity, setQuantity] = useState(1);
+    const incrementQuantity = () => {
+        if (quantity < 10) {
+            setQuantity(quantity + 1);
+        }
+    };
+    const decrementQuantity = () => {
+        if (quantity > 1) {
+            setQuantity(quantity - 1);
+        }
+    };
     return (
         <div className={ProductStyles.productGap}>
             <div className={ProductStyles.productContainer}>
                 <div
                     className={ProductStyles.productImage}
-                    // Set product image as the background, using the first image from the list
                     style={{
                         backgroundImage: `url(${productData?.value?.images[0]})`,
                         backgroundSize: 'cover',
-                    }}
-                >
-                </div>
+                    }}></div>
                 <div className={ProductStyles.productInfo}>
                     <div className={ProductStyles.productTitle}>
                         {productData?.value?.title}
                     </div>
                     <div className={ProductStyles.productPrice}>
-                        {productData?.value?.price ? `$${productData.value.price / 100}` : ''}
+                        {productData?.value?.price? `$${productData.value.price / 100}`: ''}
                     </div>
                     <div className={ProductStyles.productDetailItem}>
-                        {productData?.value?.tags[4] ? `Color: ${productData.value.tags[4]}` : ''}
+                        {productData?.value?.tags[4]? `Color: ${productData.value.tags[4]}`: ''}
                     </div>
                     <div className={ProductStyles.productDetailItem}>
-                        {productData?.value?.options[0]?.values[0] ? `Size: M` : ''}
+                        {productData?.value?.options[0]?.values[0]? `Size: M`: ''}
                     </div>
                     <div className={ProductStyles.quantitySelector}>
-                        <div className={ProductStyles.quantityButton}>-</div>
-                        <div>1</div>
-                        <div className={ProductStyles.quantityButton}>+</div>
+                        <div className={ProductStyles.quantityButton} onClick={decrementQuantity}>-</div>
+                        <div>{quantity}</div>
+                        <div className={ProductStyles.quantityButton} onClick={incrementQuantity}>+</div>
                     </div>
-                    <div className={ProductStyles.addToCartButton}>Add to cart</div>
+                    <div className={ProductStyles.addToCartButton}> Add to cart</div>
                     <div className={ProductStyles.buyNowButton}>Buy it now</div>
                     <div
                         className={ProductStyles.productDescription}
-                        dangerouslySetInnerHTML={{ __html: productData?.value?.description || '' }}
-                    ></div>
+                        dangerouslySetInnerHTML={{ __html: productData?.value?.description || '',}}></div>
                 </div>
             </div>
         </div>
