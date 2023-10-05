6ab255f12a42fb4a80b8d2936d848dcf77b8ef72
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-10-05 12:45:42 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import { NavBar } from "./NavBar.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>
 );
