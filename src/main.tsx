f4dae953dd8a681a7b987ee39fce3ffbb8fe55d8
Kanishk Bhatia <bhatia.ka@northeastern.edu>
2023-10-05 16:23:08 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { NavBar } from "./NavBar.tsx";
 import { Product } from "./Product.tsx";
+import { Carousel } from "./Carousel.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NavBar />
     <Product />
+    <Carousel title={"More of this style"} />
+    <Carousel title={"You might also like"} />
   </React.StrictMode>
 );
